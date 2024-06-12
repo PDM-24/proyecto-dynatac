3e30091382a19059ec123ac9f592e6f6978f8597
Fabio Mijango <105021697+FabioMijango@users.noreply.github.com>
2024-06-12 16:58:45 -0600

    Delete SocialBrewApp directory

--- SocialBrewApp/app/src/main/java/com/permafrost/socialbrewapp/ui/navigation/ScreenRoute.kt
+++
@@ -1,6 +0,0 @@
-package com.permafrost.socialbrewapp.ui.navigation
-
-sealed class ScreenRoute(val route: String) {
-    object Login : ScreenRoute("login")
-    object Home : ScreenRoute("home")
-}
--- SocialBrewApp/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-
-plugins {
-    alias(libs.plugins.androidApplication) apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("com.google.devtools.ksp") version "1.9.23-1.0.20"
-}
